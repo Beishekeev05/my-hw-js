3f14d148f0e569fcbbd8f6aeee8d70e6b90a47a6
Syimyk Beishekeev <syjmyk907@gmail.com>
2024-03-20 00:57:36 +0600

    bhsa

---
+++ HW-26/script/data.js
@@ -0,0 +1,8 @@
+const dataInter = fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    // console.log('Error', error);
+  });
